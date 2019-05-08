1fd952de28b35e2d61da2e210d5b906f78a8167d
Waldemar <waldemar.felde@outlook.com>
2019-05-08 09:13:08 +0200

    Spring Core User Service initial commit

---
+++ webshop-core-user-service/src/main/java/de/hska/iwi/vslab/webshopcoreuserservice/WebshopCoreUserServiceApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.webshopcoreuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopCoreUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopCoreUserServiceApplication.class, args);
+	}
+
+}
