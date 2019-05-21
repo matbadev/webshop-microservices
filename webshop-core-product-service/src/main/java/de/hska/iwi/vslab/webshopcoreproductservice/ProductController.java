a0c060ed696f8b7bc5895184f0ba469dd01982f4
Matthieu Laqua <mlmatthieul@gmail.com>
2019-05-22 00:50:25 +0200

    Implement product service

--- webshop-core-product-service/src/main/java/de/hska/iwi/vslab/webshopcoreproductservice/ProductController.java
+++ webshop-core-product-service/src/main/java/de/hska/iwi/vslab/webshopcoreproductservice/ProductController.java
@@ -1,21 +1,58 @@
 package de.hska.iwi.vslab.webshopcoreproductservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping(path = "/")
 public class ProductController {
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-    @GetMapping(path = "/hello")
-    public @ResponseBody String helloworld() {
-        return "Hello World!";
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
+    @GetMapping(path = "/products")
+    public @ResponseBody
+    Iterable<Product> getProducts(@RequestParam(defaultValue = "") String text,
+                                  @RequestParam(defaultValue = "-1e20") double minPrice,
+                                  @RequestParam(defaultValue = "1e20") double maxPrice) {
+        return StreamSupport.stream(productRepository.findAll().spliterator(), false)
+                .filter(product -> (product.getName().contains(text) || product.getDetails().contains(text))
+                        && product.getPrice() >= minPrice
+                        && product.getPrice() <= maxPrice)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping(path = "/products")
+    public @ResponseBody
+    Product newProduct(@RequestBody Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+
+    @GetMapping(path = "/products/{productId}")
+    public @ResponseBody
+    Product getProductById(@PathVariable int productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping(path = "/products/{productId}")
+    public ResponseEntity<Product> deleteProductById(@PathVariable int productId) {
+        if (productRepository.findById(productId).isPresent()) {
+            productRepository.deleteById(productId);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 }
