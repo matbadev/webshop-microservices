48b8fc7aef4cd5cc830755100f8fc5ce7461a7dc
Matthieu Laqua <mlmatthieul@gmail.com>
2019-05-22 12:47:31 +0200

    Add initial composite service project files

---
+++ webshop-composite-inventory-service/src/main/java/de/hska/iwi/vslab/webshopcompositeinventoryservice/WebshopCompositeInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.webshopcompositeinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopCompositeInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopCompositeInventoryServiceApplication.class, args);
+	}
+
+}
