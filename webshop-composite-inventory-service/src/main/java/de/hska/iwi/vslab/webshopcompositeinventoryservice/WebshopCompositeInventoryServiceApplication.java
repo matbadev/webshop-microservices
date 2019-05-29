19cbafb96e5b5c5a58cc029c6abb9a633d41eb46
Matthieu Laqua <mlmatthieul@gmail.com>
2019-05-29 10:21:41 +0200

    Update inventory service with discovery

--- webshop-composite-inventory-service/src/main/java/de/hska/iwi/vslab/webshopcompositeinventoryservice/WebshopCompositeInventoryServiceApplication.java
+++ webshop-composite-inventory-service/src/main/java/de/hska/iwi/vslab/webshopcompositeinventoryservice/WebshopCompositeInventoryServiceApplication.java
@@ -2,8 +2,16 @@ package de.hska.iwi.vslab.webshopcompositeinventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@RibbonClient("inventory-proxy")
 public class WebshopCompositeInventoryServiceApplication {
 
 	public static void main(String[] args) {
