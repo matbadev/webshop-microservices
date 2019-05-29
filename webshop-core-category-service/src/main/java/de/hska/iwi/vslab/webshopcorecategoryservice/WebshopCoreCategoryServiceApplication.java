5bd3b1f3a28410a435d83b7558c9f64426acc03b
Waldemar <waldemar.felde@outlook.com>
2019-05-29 09:39:37 +0200

    Core-Services edited for Eureka Server

--- webshop-core-category-service/src/main/java/de/hska/iwi/vslab/webshopcorecategoryservice/WebshopCoreCategoryServiceApplication.java
+++ webshop-core-category-service/src/main/java/de/hska/iwi/vslab/webshopcorecategoryservice/WebshopCoreCategoryServiceApplication.java
@@ -2,8 +2,10 @@ package de.hska.iwi.vslab.webshopcorecategoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebshopCoreCategoryServiceApplication {
 
 	public static void main(String[] args) {
