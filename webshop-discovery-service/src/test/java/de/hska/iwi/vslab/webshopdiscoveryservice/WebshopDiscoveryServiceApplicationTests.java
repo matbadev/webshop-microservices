f4f24faa0f25f6e430e93298007daac32ab31432
matthias-baeuerle <Ma.Baeuerle@gmx.de>
2019-05-29 08:58:04 +0200

    Add discovery service

---
+++ webshop-discovery-service/src/test/java/de/hska/iwi/vslab/webshopdiscoveryservice/WebshopDiscoveryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.hska.iwi.vslab.webshopdiscoveryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebshopDiscoveryServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
