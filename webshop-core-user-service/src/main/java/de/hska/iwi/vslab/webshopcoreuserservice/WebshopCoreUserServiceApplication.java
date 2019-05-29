2d798edefec86651788f45b542b8b8b33f14f595
matthias-baeuerle <Ma.Baeuerle@gmx.de>
2019-05-29 09:35:14 +0200

    Add discovery to user service

--- webshop-core-user-service/src/main/java/de/hska/iwi/vslab/webshopcoreuserservice/WebshopCoreUserServiceApplication.java
+++ webshop-core-user-service/src/main/java/de/hska/iwi/vslab/webshopcoreuserservice/WebshopCoreUserServiceApplication.java
@@ -2,8 +2,10 @@ package de.hska.iwi.vslab.webshopcoreuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebshopCoreUserServiceApplication {
 
 	public static void main(String[] args) {
