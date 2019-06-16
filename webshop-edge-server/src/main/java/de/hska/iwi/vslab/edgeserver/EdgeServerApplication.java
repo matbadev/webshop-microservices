eb374aaefdde08aba346cbb23293816a6830a075
Waldemar <waldemar.felde@outlook.com>
2019-06-16 17:17:32 +0200

    Added Zuul Sever

---
+++ webshop-edge-server/src/main/java/de/hska/iwi/vslab/edgeserver/EdgeServerApplication.java
@@ -0,0 +1,15 @@
+package de.hska.iwi.vslab.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServerApplication.class, args);
+	}
+
+}
