ca16a1839ad3f619571fb0f2f1b6f48532ab06d1
matthias-baeuerle <Ma.Baeuerle@gmx.de>
2019-06-18 16:12:48 +0200

    Add service for Hystrix dashboard

---
+++ webshop-hystrix-dashboard-service/src/main/java/de/hska/iwi/vslab/webshophystrixdashboardservice/WebshopHystrixDashboardServiceApplication.java
@@ -0,0 +1,15 @@
+package de.hska.iwi.vslab.webshophystrixdashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class WebshopHystrixDashboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopHystrixDashboardServiceApplication.class, args);
+    }
+
+}
