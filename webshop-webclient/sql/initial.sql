f23523f5e678a70e32bc6d87421e7d54a005879c
matthias-baeuerle <Ma.Baeuerle@gmx.de>
2019-06-20 16:00:58 +0200

    Remove SQL files in webclient

--- webshop-webclient/sql/initial.sql
+++
@@ -1,4 +0,0 @@
-insert into `role` (`level1`, `type`) values(0, 'admin');
-insert into `role` (`level1`, `type`) values(1, 'user');
-
-insert into `customer` (`name`, `lastname`, `password`, `username`, `role`) values('admin', 'admin', 'admin', 'admin', 1);
