b417743d10d55475e95a73b6f109198f8ec40ce2
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2019-06-26 09:56:08 +0200

    Start implementing Auth

--- webshop-edge-service/src/main/java/de/hska/iwi/vslab/edgeserver/EdgeServerApplication.java
+++ webshop-edge-service/src/main/java/de/hska/iwi/vslab/edgeserver/EdgeServerApplication.java
@@ -1,17 +1,102 @@
 package de.hska.iwi.vslab.edgeserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableResourceServer
 public class EdgeServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdgeServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServerApplication.class, args);
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Bean
+        public TokenStore tokenStore() {
+            return new InMemoryTokenStore();
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+            endpoints.tokenStore(tokenStore())
+                    .authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("webshop-webclient")
+                    .secret("{noop}secret")
+                    .authorizedGrantTypes("client_credentials", "password")
+                    .scopes("read");
+
+        }
+
+    }
+
+    @Configuration
+    @EnableResourceServer
+    public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.anonymous().disable()
+                    .authorizeRequests()
+                    .antMatchers("/user-api/**").authenticated()
+                    .antMatchers("/inventory-api/**").authenticated()
+                    .and()
+                    .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+        }
+
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private WebshopUserDetailsService userDetailsService;
+
+        @Override
+        @Bean
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+        @Autowired
+        public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService);
+        }
+    }
 
 }
