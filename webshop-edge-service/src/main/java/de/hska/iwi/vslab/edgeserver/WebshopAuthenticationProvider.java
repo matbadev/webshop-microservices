fe9f5441aa194f157032a7bc40e891c6e0c79092
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2019-07-01 19:39:29 +0200

    Add password checking in edge-service

--- webshop-edge-service/src/main/java/de/hska/iwi/vslab/edgeserver/WebshopAuthenticationProvider.java
+++ webshop-edge-service/src/main/java/de/hska/iwi/vslab/edgeserver/WebshopAuthenticationProvider.java
@@ -2,6 +2,7 @@ package de.hska.iwi.vslab.edgeserver;
 
 import de.hska.iwi.vslab.edgeserver.model.UserCore;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import static java.util.Objects.requireNonNull;
@@ -27,20 +29,31 @@ public class WebshopAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        UserCore[] users = requireNonNull(restTemplate.getForObject(UserController.API_USERS, UserCore[].class));
-        logger.info("Loaded " + users.length + " users: " + Arrays.deepToString(users));
         String username = authentication.getName();
-        for (UserCore user : users) {
-            if (user.getUsername().equals(username)) {
-                return new UsernamePasswordAuthenticationToken(
-                        user.getUsername(),
-                        user.getPassword(),
-                        List.of(user)
-                );
-            }
+        String password = (String) authentication.getCredentials();
+        Optional<UserCore> userOpt = getUserByUsername(username);
+        UserCore user = userOpt.orElseThrow(() -> new UsernameNotFoundException("User with name '" + username + "' not found"));
+
+        if (!user.getPassword().equals(password)) {
+            throw new BadCredentialsException("Wrong password for user '" + username + '"');
         }
 
-        throw new UsernameNotFoundException("User with name '" + username + "' not found");
+        return new UsernamePasswordAuthenticationToken(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(user)
+        );
+    }
+
+    private Optional<UserCore> getUserByUsername(String username) {
+        UserCore[] users = requireNonNull(restTemplate.getForObject(UserController.API_USERS, UserCore[].class));
+        logger.info("Loaded " + users.length + " users: " + Arrays.deepToString(users));
+        for (UserCore user : users) {
+            if (user.getUsername().equals(username)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
     }
 
 }
