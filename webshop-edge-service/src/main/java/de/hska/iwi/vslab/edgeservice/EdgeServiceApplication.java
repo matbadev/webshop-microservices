ae337a22bd0d5708fe95688a549808a118f3b2a2
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2019-07-23 19:17:19 +0200

    Rewrite edge service (authorization is now in separate service)

---
+++ webshop-edge-service/src/main/java/de/hska/iwi/vslab/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,42 @@
+package de.hska.iwi.vslab.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableOAuth2Sso
+public class EdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServiceApplication.class, args);
+    }
+
+    @Configuration
+    @EnableResourceServer
+    public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
+                    .authorizeRequests()
+                    /**/.antMatchers("/auth/**").permitAll()
+                    /**/.antMatchers("/**").authenticated()
+                    .and()
+                    /**/.exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+        }
+
+    }
+
+}
--- webshop-edge-service/src/test/java/de/hska/iwi/vslab/edgeservice/EdgeServiceApplicationTests.java
+++ webshop-edge-service/src/test/java/de/hska/iwi/vslab/edgeservice/EdgeServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package de.hska.iwi.vslab.edgeserver;
+package de.hska.iwi.vslab.edgeservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EdgeServerApplicationTests {
+public class EdgeServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
