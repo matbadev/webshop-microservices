ac4e1f1a4a4f19c1c64aee8083b8f788ac3ac9c3
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2019-07-23 19:21:20 +0200

    Secure user service

--- webshop-core-user-service/src/main/java/de/hska/iwi/vslab/webshopcoreuserservice/WebshopCoreUserServiceApplication.java
+++ webshop-core-user-service/src/main/java/de/hska/iwi/vslab/webshopcoreuserservice/WebshopCoreUserServiceApplication.java
@@ -3,13 +3,38 @@ package de.hska.iwi.vslab.webshopcoreuserservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class WebshopCoreUserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebshopCoreUserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopCoreUserServiceApplication.class, args);
+    }
+
+    @Configuration
+    @EnableResourceServer
+    public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
+                    .authorizeRequests().anyRequest().fullyAuthenticated();
+        }
+
+    }
+
+    @Configuration
+    @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+    public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+    }
 
 }
--- webshop-core-user-service/src/main/java/de/hska/iwi/vslab/webshopcoreuserservice/api/UserController.java
+++ webshop-core-user-service/src/main/java/de/hska/iwi/vslab/webshopcoreuserservice/api/UserController.java
@@ -11,15 +11,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.logging.Logger;
 
-@Controller
+@RestController
 @EnableHystrix
 @RequestMapping("/")
 public class UserController {
@@ -42,14 +49,15 @@ public class UserController {
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2")
     })
     @GetMapping("users")
-    public ResponseEntity<Iterable<User>> getAllUsers() {
-
+    @RolesAllowed({"ROLE_DEBUG"})
+    public Iterable<User> getAllUsers() {
+        Iterable<User> users = userRepository.findAll();
         userCache.clear();
-        userRepository.findAll().forEach(user -> userCache.put(user.getId(), user));
-
-        return ResponseEntity.ok(userRepository.findAll());
+        users.forEach(user -> userCache.put(user.getId(), user));
+        return users;
     }
 
+    @SuppressWarnings("unused")
     public ResponseEntity<Iterable<User>> getAllUsersCache() {
         return ResponseEntity.ok(userCache.values());
     }
@@ -58,6 +66,7 @@ public class UserController {
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2")
     })
     @PostMapping("users")
+    @RolesAllowed({"ROLE_USER"})
     public ResponseEntity<User> createUser(@RequestBody @Valid NewUser newUser) {
         Role role = roleRepository.findByTypeEquals(newUser.roletype)
                 .orElseGet(() -> createRole(newUser.roletype));
@@ -66,6 +75,7 @@ public class UserController {
         return ResponseEntity.ok(createdUser);
     }
 
+    @SuppressWarnings("unused")
     public ResponseEntity<User> createUserCache(@RequestBody @Valid NewUser newUser) {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(null);
     }
@@ -80,13 +90,19 @@ public class UserController {
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2")
     })
     @GetMapping("users/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable("userId") int userId) {
-
-        userCache.putIfAbsent(userId, userRepository.findById(userId).get());
-        return ResponseEntity.of(userRepository.findById(userId));
+    @RolesAllowed({"ROLE_DEBUG"})
+    public ResponseEntity<User> getUserById(@PathVariable("userId") Integer userId) {
+        Optional<User> userOpt = userRepository.findById(userId);
+        if (userOpt.isPresent()) {
+            userCache.putIfAbsent(userId, userOpt.get());
+            return ResponseEntity.ok(userOpt.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    public ResponseEntity<User> getUserByIdCache(@PathVariable("userId") int userId) {
+    @SuppressWarnings("unused")
+    public ResponseEntity<User> getUserByIdCache(Integer userId) {
         try {
             User user = userCache.get(userId);
             return ResponseEntity.ok(user);
@@ -99,7 +115,8 @@ public class UserController {
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2")
     })
     @DeleteMapping("users/{userId}")
-    public ResponseEntity deleteUserById(@PathVariable("userId") int userId) {
+    @RolesAllowed({"ROLE_DEBUG"})
+    public ResponseEntity deleteUserById(@PathVariable("userId") Integer userId) {
         if (userRepository.existsById(userId)) {
             userRepository.deleteById(userId);
             return ResponseEntity.noContent().build();
@@ -108,7 +125,8 @@ public class UserController {
         }
     }
 
-    public ResponseEntity deleteUserByIdCache(@PathVariable("userId") int userId) {
+    @SuppressWarnings("unused")
+    public ResponseEntity deleteUserByIdCache(Integer userId) {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(null);
     }
 
@@ -116,6 +134,7 @@ public class UserController {
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2")
     })
     @GetMapping("roles")
+    @RolesAllowed({"ROLE_DEBUG"})
     public ResponseEntity<Iterable<Role>> getAllRoles() {
 
         roleCache.clear();
@@ -124,7 +143,9 @@ public class UserController {
         return ResponseEntity.ok(roleRepository.findAll());
     }
 
+    @SuppressWarnings("unused")
     public ResponseEntity<Iterable<Role>> getAllRolesCache() {
         return ResponseEntity.ok(roleCache.values());
     }
+
 }
